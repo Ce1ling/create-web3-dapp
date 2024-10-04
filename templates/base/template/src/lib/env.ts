c61f81db7103a4ac69427a0e66ea0c50c0d82012
l1en <2134045201@qq.com>
2024-10-04 22:09:36 +0800

    fix: public env variables naming

--- templates/base/template/src/lib/env.ts
+++ templates/base/template/src/lib/env.ts
@@ -1,4 +1,4 @@
 export const dotenv = {
-  isDev: process.env.NEXT_ENV === 'dev',
-  isProd: process.env.NEXT_ENV === 'prod',
+  isDev: process.env.NEXT_PUBLIC_NEXT_ENV === 'dev',
+  isProd: process.env.NEXT_PUBLIC_NEXT_ENV === 'prod',
 }
