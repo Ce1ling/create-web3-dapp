60d2913d4cb0e0bca76a94e4c663523276bbd2f3
l1en <2134045201@qq.com>
2024-10-30 10:58:05 +0800

    feat(useResponsive): support custom config

--- templates/base/template/src/hooks/use-responsive.ts
+++ templates/base/template/src/hooks/use-responsive.ts
@@ -1,15 +1,24 @@
 import { useMediaQuery } from 'react-responsive'
 
-import { responsiveConfig } from '@/config/responsive'
+// Same as tailwind's responsive. See https://tailwindcss.com/docs/responsive-design
+export const responsiveConfig = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  xxl: 1536,
+}
 
-export const useResponsive = () => ({
-  isSm: useMediaQuery({ minWidth: responsiveConfig.isSm }),
-  isMd: useMediaQuery({ minWidth: responsiveConfig.isMd }),
-  isLg: useMediaQuery({ minWidth: responsiveConfig.isLg }),
-  isXl: useMediaQuery({ minWidth: responsiveConfig.isXl }),
+export const useResponsive = (config?: Partial<typeof responsiveConfig>) => ({
+  isSm: useMediaQuery({ minWidth: config?.sm || responsiveConfig.sm }),
+  isMd: useMediaQuery({ minWidth: config?.md || responsiveConfig.md }),
+  isLg: useMediaQuery({ minWidth: config?.lg || responsiveConfig.lg }),
+  isXl: useMediaQuery({ minWidth: config?.xl || responsiveConfig.xl }),
+  is2Xl: useMediaQuery({ minWidth: config?.xxl || responsiveConfig.xxl }),
 
-  isMaxSm: useMediaQuery({ maxWidth: responsiveConfig.isSm }),
-  isMaxMd: useMediaQuery({ maxWidth: responsiveConfig.isMd }),
-  isMaxLg: useMediaQuery({ maxWidth: responsiveConfig.isLg }),
-  isMaxXl: useMediaQuery({ maxWidth: responsiveConfig.isXl }),
+  isMaxSm: useMediaQuery({ maxWidth: config?.sm || responsiveConfig.sm }),
+  isMaxMd: useMediaQuery({ maxWidth: config?.md || responsiveConfig.md }),
+  isMaxLg: useMediaQuery({ maxWidth: config?.lg || responsiveConfig.lg }),
+  isMaxXl: useMediaQuery({ maxWidth: config?.xl || responsiveConfig.xl }),
+  isMax2Xl: useMediaQuery({ maxWidth: config?.xxl || responsiveConfig.xxl }),
 })
