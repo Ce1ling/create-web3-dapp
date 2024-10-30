33337f139894b6553a4364d091ca0c51f22a9875
l1en <2134045201@qq.com>
2024-10-30 11:08:24 +0800

    chore: add TODO for contract

--- templates/evm/contract/abi/example.ts
+++ templates/evm/contract/abi/example.ts
@@ -1,1 +1,2 @@
+// TODO: YOUR_CONTRACT_ABI. Don't forget to change file name.
 export const exampleAbi = [] as const
--- templates/evm/contract/address.ts
+++ templates/evm/contract/address.ts
@@ -1,7 +1,13 @@
 import { dotenv } from '@/lib/env'
 
-const prod = {}
+const prod = {
+  // TODO: YOUR_CONTRACT_ADDRESS
+  contractA: '0x000',
+}
 
-const dev = {}
+const dev = {
+  // TODO: YOUR_CONTRACT_ADDRESS
+  contractA: '0x000',
+}
 
 export const contractAddr = dotenv.isDev ? dev : prod
